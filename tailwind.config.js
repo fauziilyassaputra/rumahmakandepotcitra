45a6df1c0a642b3307c071c9ab2459cb5689b0cd
fauzi <fauziilyassaputra@gmail.com>
2024-10-15 19:01:35 +0700

    kasih jquery

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}", "index.html"],
   theme: {
     // screens: {
     //   ssm: "350px",
