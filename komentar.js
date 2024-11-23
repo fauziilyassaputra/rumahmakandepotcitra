27ab957b1d7216eaac68d3369149366cf8251a06
fauzi <fauziilyassaputra@gmail.com>
2024-11-23 16:55:02 +0700

    ubah transition

---
+++ komentar.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $("#tombol2").on("click", function () {
+    $("#isikomentar").html(
+      '<img src="public/komentator/puteri.png" alt="" class="w-44 inline-block"><p class="sm:text-lg 2xl:text-xl 2xl:leading-relaxed"id="isikomentar"><span class="block font-bold sm:text-xl 2xl:text-2xl">Puteri Qotrina</span> Makanan seafood enak bumbu rumahan harga bersahabat banget. Potongan ikannya gede2. Fresh. Ada jg non seafood. RM oldschool tp tetap survive Krn RASA </p>'
+    );
+  });
+  $("#tombol1").on("click", function () {
+    $("#isikomentar").html(
+      '<img src="public/komentator/matt.png" alt="" class="w-52 inline-block"><p class="sm:text-lg 2xl:text-xl 2xl:leading-relaxed"id="isikomentar"><span class="block font-bold sm:text-xl 2xl:text-2xl">Matt Journey</span> Masakan khas madura yang susah untuk tidak mampir jika melewati jalan arah ke Pamekasan (jika dari arah barat) atau jalan arah ke Sampang (jika dari arah timur) </p>'
+    );
+  });
+  $("#tombol3").on("click", function () {
+    $("#isikomentar").html(
+      '<img src="public/komentator/arifin.png" alt="" class="w-52 inline-block"><p class="sm:text-lg 2xl:text-xl 2xl:leading-relaxed"id="isikomentar"><span class="block font-bold sm:text-xl 2xl:text-2xl">arifin waluya</span> Tempat ini easy to notice Masakannya cocok dengan lidah saya lidah jawa harga Alhamdulillah bersahabat </p>'
+    );
+  });
+  $("#tombol4").on("click", function () {
+    $("#isikomentar").html(
+      '<img src="public/komentator/rezeki.png" alt="" class="w-52 inline-block"><p class="sm:text-lg 2xl:text-xl 2xl:leading-relaxed"id="isikomentar"><span class="block font-bold sm:text-xl 2xl:text-2xl">Awal M Rezeki</span> Harga murah, rasa enak, variasi menu banyak, lokasi strategis, tempat bersih dan luas... Yg terpenting lagi penjualnya ramah </p>'
+    );
+  });
+  $("#tombol5").on("click", function () {
+    $("#isikomentar").html(
+      '<img src="public/komentator/saifudin.png" alt="" class="w-44 inline-block"><p class="sm:text-lg 2xl:text-xl 2xl:leading-relaxed"id="isikomentar"><span class="block font-bold sm:text-xl 2xl:text-2xl">Saifudin Gituloh</span> ...disediakan tempat lesehan maupun duduk. Untuk harga cukup murah.... Waktu saya berkunjung disini tidak ada tukang parkir sehingga bebas parkir. </p>'
+    );
+  });
+});
